c1670d032d5b9c85690b7b3004a5c0d81b1ced70
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2022-06-05 21:58:03 -0300

    refactor, feat and style: update first page style

--- js/records.js
+++ js/records.js
@@ -1,32 +1,40 @@
 const nameUser = document.getElementById('nameField');
 const buttonName = document.getElementById('playButton');
-const score = document.getElementById('players');
+const score = document.getElementById('playerList');
 const meuStorage = sessionStorage;
 
 let users = [];
-if(meuStorage.getItem("nameUser")) users = JSON.parse(meuStorage.getItem("nameUser"));
-for(const user of users) {
-    const userName = document.createElement('p');
-    userName.innerHTML = user
+if (meuStorage.getItem('nameUser'))
+  users = JSON.parse(meuStorage.getItem('nameUser'));
+else {
+  const noPlayers = document.createElement('li');
+  noPlayers.innerHTML = 'Nenhum jogador registrado!';
 
-    score.appendChild(userName);
+  score.appendChild(noPlayers);
+}
+for (const user of users) {
+  const userName = document.createElement('li');
+  userName.innerHTML = user;
+
+  score.appendChild(userName);
 }
 
 function saveName() {
-    users.push(nameUser.value);
-    console.log(users);
-    meuStorage.setItem('nameUser', JSON.stringify(users));
-    nameUser.value = "";
+  users.push(nameUser.value);
+  console.log(users);
+  meuStorage.setItem('nameUser', JSON.stringify(users));
+  nameUser.value = '';
 
-    window.location.replace("/initialScreen.html");
+  window.location.replace('/initialScreen.html');
 }
 
 function typeWrite(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ''
-    textoArray.forEach((letra, i) =>{
-        setTimeout(() => elemento.innerHTML += letra, 75 * i);
-    }) 
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+  }) 
 }
+
 const titulo = document.querySelector('.maquina-escrever');
 typeWrite(titulo);
